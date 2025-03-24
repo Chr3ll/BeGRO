24e9838a89ebb57f7347eeab4129cd8e8ab6ff5e
Nabigh Nugdallah <nabighnugdallah@gmail.com>
2025-03-24 06:08:08 +0000

    Update index.js

--- spotless hungry crocodile-react/src/index.js
+++ spotless hungry crocodile-react/src/index.js
@@ -9,16 +9,17 @@ import {
 
 import './style.css'
 //import NotFound1, { NotFound } from './views/not-found'
-import NotFound1, { NotFound } from '/views/not-found.js'
+//import NotFound1, { NotFound } from '/views/not-found.js'
+import NotFound1 from './views/not-found'
 import Home from './views/home'
 
 const App = () => {
   return (
     <Router>
       <Switch>
-        <Route component={NotFound1} exact path="/not-found" />
-        <Route component={Home} exact path="/" />
-        <Route component={NotFound} path="**" />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/not-found" component={NotFound1} />
+        <Route component={NotFound1} />
         <Redirect to="**" />
       </Switch>
     </Router>
